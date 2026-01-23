df147325b08f491ef7f889da0b0b596ded59e75a
ayush00git <ayush.2007.iit@gmail.com>
2026-01-23 19:38:15 +0530

    added a preloader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
-import "./globals.css"; 
-import Navbar from "../components/Navbar"; 
+import "./globals.css";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+import Preloader from "../components/Preloader";
 
 export const metadata = {
   title: "App Team",
@@ -15,12 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-    <meta name="google-site-verification" content="wSkX_8y2bFPHN0i7D-DUVeKV2QAfnC2NVDFgCh0Cnhc" />
-    </head>
+      <head>
+        <meta name="google-site-verification" content="wSkX_8y2bFPHN0i7D-DUVeKV2QAfnC2NVDFgCh0Cnhc" />
+      </head>
       <body>
+        <Preloader />
         <Navbar />
-          <main>{children}</main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
